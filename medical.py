6dd0dcf2003bde85e51017dc77acae4046ec959a
Vignesh Suresh <61021342+Vignesh424@users.noreply.github.com>
2025-04-05 14:17:31 +0530

    Update medical.py

--- medical.py
+++ medical.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv,find_dotenv
 from PIL import Image
 load_dotenv()
-genai.configure(api_key="AIzaSyBZUa2gpJGrSbOBzG23dE8LNhPmY4lgdrg") #Dont use this API key
+genai.configure(api_key,os.config=GOOGLE_API_KEY) 
 
 
 def get_gemini_response(input,image):
